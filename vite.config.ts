a13cb12d4bb877f78afd32e6a17a7bb28a1661a1
harsh-1315 <harshhere12345@gmail.com>
2026-06-24 19:05:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
-import glsl from 'vite-plugin-glsl'
+import glsl from "vite-plugin-glsl"
 
 export default defineConfig({
-  base: './',
-  plugins: [inspectAttr(), react(), glsl()],
+  base: "./",
+  plugins: [react(), glsl()],
   server: {
     port: 3000,
   },
